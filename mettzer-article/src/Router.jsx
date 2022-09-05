e0baed8490f446b4daa40be287c906af139fae87
caio300 <caioqs.info@gmail.com>
2022-09-05 16:19:09 -0300

    Feito layout das rotas

--- mettzer-article/src/Router.jsx
+++ mettzer-article/src/Router.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 import { Favorite } from './Pages/Favorite'
 import { Home } from './Pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/favorite" element={<Favorite />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Route>
     </Routes>
   )
 }
